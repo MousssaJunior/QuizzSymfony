d2c6aba1a5d1e7ab22d554cdbdcb7546b84bdb73
moussa <moussa-junior.fofana@epitech.eu>
2024-05-13 17:17:11 +0200

    fix: select instead of li home/twig

--- quizz-symfony/src/Controller/CategorieController.php
+++ quizz-symfony/src/Controller/CategorieController.php
@@ -6,17 +6,19 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\CategorieRepository;
+use App\Repository\QuestionRepository;
 
 class CategorieController extends AbstractController
 {
     #[Route('/', name: 'app_categorie')]
-    public function index(CategorieRepository $repositoryCategorie): Response
+    public function showCategorie(CategorieRepository $repositoryCategorie , QuestionRepository $repositoryQuestion): Response
     {
         $categories = $repositoryCategorie->findAll();
     
         return $this->render('home/index.html.twig', [
             'categories' => $categories,
         ]);
+
     }
-    
+      
 }
