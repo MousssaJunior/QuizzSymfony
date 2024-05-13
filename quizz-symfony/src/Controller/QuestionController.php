1324fdc099c7ebe42fb97e934b748f5e9faf3b35
moussa <moussa-junior.fofana@epitech.eu>
2024-05-13 10:35:08 +0200

    add: in QuestionController function show question

--- quizz-symfony/src/Controller/QuestionController.php
+++ quizz-symfony/src/Controller/QuestionController.php
@@ -5,14 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-
+use App\Repository\QuestionRepository;
 class QuestionController extends AbstractController
 {
     #[Route('/question', name: 'app_question')]
-    public function index(): Response
+    public function showQuestion(QuestionRepository $questionRepo): Response
     {
+        $question = $questionRepo->findAll();
+    
         return $this->render('question/index.html.twig', [
-            'controller_name' => 'QuestionController',
+            'question' => $question,
         ]);
     }
 }
