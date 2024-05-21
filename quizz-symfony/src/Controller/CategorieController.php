e9b2933f7e4d8d0dd2055006c4cfbc4a3c162454
moussa <moussa-junior.fofana@epitech.eu>
2024-05-21 16:25:58 +0200

    nothing change

--- quizz-symfony/src/Controller/CategorieController.php
+++ quizz-symfony/src/Controller/CategorieController.php
@@ -8,11 +8,12 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\CategorieRepository;
 use App\Repository\QuestionRepository;
 use App\Repository\ReponseRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class CategorieController extends AbstractController
 {
     #[Route('/', name: 'app_categorie')]
-    public function showCategorie(CategorieRepository $repositoryCategorie , QuestionRepository $repositoryQuestion): Response
+    public function showCategorie(CategorieRepository $repositoryCategorie , QuestionRepository $repositoryQuestion,): Response
     {
         $categories = $repositoryCategorie->findAll();
     
@@ -23,11 +24,17 @@ class CategorieController extends AbstractController
     }
     
     #[Route('/categorie/{id}', name: 'app_quizz')]
-    public function showQuizz($id, CategorieRepository $repositoryCategorie, QuestionRepository $repositoryQuestion, ReponseRepository $repositoryReponse): Response
+    public function showQuizz($id, CategorieRepository $repositoryCategorie, QuestionRepository $repositoryQuestion, ReponseRepository $repositoryReponse,Request $request): Response
     {  
         $categorie = $repositoryCategorie->find($id);
         $questions = $repositoryQuestion->findBy(['id_categorie' => $categorie]);
         $reponses =  $repositoryReponse->findBy(['id_question' => $questions]);
+
+        // $monChamp = $request->request->get('input');
+        // $nomDuChamp = $monChamp->getName('radio');
+
+        // var_dump($monChamp);
+        // echo $nomduChamp;
 
         
         return $this->render('categorie/index.html.twig', [ 
--- quizz-symfony/src/Controller/ReponseController.php
+++ quizz-symfony/src/Controller/ReponseController.php
@@ -8,6 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\CategorieRepository;
 use App\Repository\QuestionRepository;
 use App\Repository\ReponseRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 class ReponseController extends AbstractController
 {
@@ -20,20 +21,25 @@ class ReponseController extends AbstractController
     }
 
     #[Route('/categorie/{id}', name: 'check_reponse')]
-    public function checkQuizz($id, CategorieRepository $repositoryCategorie, QuestionRepository $repositoryQuestion, ReponseRepository $repositoryReponse): Response
+    public function checkQuizz($id, CategorieRepository $repositoryCategorie, QuestionRepository $repositoryQuestion, ReponseRepository $repositoryReponse, Request $request): Response
     {  
         $categorie = $repositoryCategorie->find($id);
         $questions = $repositoryQuestion->findBy(['id_categorie' => $categorie]);
         $reponses =  $repositoryReponse->findBy(['id_question' => $questions]);
         $true = $repositoryReponse->findby(['reponse_expected', $reponses]);
+
+       
+        $monChamp = $request->request->get('radio');
+        $nomDuChamp = $monChamp->getName('radio');
         
-        return $this->render('categorie/index.html.twig', [ 
+        echo $monChamp;
+        echo $nomduChamp;
+
+        
+        return $this->render('reponse/index.html.twig', [ 
+          $monChamp
+        
           
-            'categorie' => $categorie,
-            'questions' => $questions,
-            'reponses' => $reponses,
-            'vrai' => $vrai
-            // dd($reponses),
         ]);
              
     }
