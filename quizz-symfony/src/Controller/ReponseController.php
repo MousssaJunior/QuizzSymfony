3357d7e52b0b8f1245ab83fefdfa05fee51aa30d
moussa <moussa-junior.fofana@epitech.eu>
2024-05-22 17:54:18 +0200

    add:count good reponse

--- quizz-symfony/src/Controller/ReponseController.php
+++ quizz-symfony/src/Controller/ReponseController.php
@@ -4,29 +4,37 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Repository\CategorieRepository;
 use App\Repository\QuestionRepository;
 use App\Repository\ReponseRepository;
 use Symfony\Component\HttpFoundation\Request;
 
+
 class ReponseController extends AbstractController
 {
-    #[Route('/reponse_back', name: 'app_reponse')]
-    public function GetResponses(Request $request): Response
-    {
-        dd($request->get("question"));
-        $reponse = $request;
-        return $this->redirectToRoute('app_reponse_display', ["reponses" => $reponse
-        ]);
+    #[Route('/reponse_back/{id_categorie}', name: 'app_reponse')]
+    public function GetResponses(Request $request,  CategorieRepository $repositoryCategorie, QuestionRepository $repositoryQuestion, ReponseRepository $repositoryReponse, $id_categorie): RedirectResponse
+    {   $reponse =  $request->get("question");
+        $value = array_values($reponse);
+        $expected = $repositoryReponse->findby(['id' => $value] );
+        $resultat = 0;
+        $AllQuestions = count($expected);
+        foreach($expected as $val){
+            if($val->getReponseExpected() == 1){
+               $resultat++;
+            }
+        }
+        return $this->redirectToRoute('app_resultat', ['resultat' => $resultat, 'AllQuestion' => $AllQuestions, 'id_categorie' => $id_categorie]);
     }
 
-    #[Route('/reponse_front/{reponses}', name: 'app_reponse_display')]
-    public function DisplayResponses(Request $request, $reponses): Response
-    {
-        return $this->render('reponse/index.html.twig', ['reponses' => $reponses
-        ]);
-    }
+    // #[Route('/reponse_front/{reponses}', name: 'app_reponse_display')]
+    // public function DisplayResponses(Request $request,  CategorieRepository $repositoryCategorie, QuestionRepository $repositoryQuestion, ReponseRepository $repositoryReponse): Response
+    // {
+    //     return $this->render('reponse/index.html.twig', ['reponses' => $reponses
+    //     ]);
+    // }
 
     // #[Route('/categorie/{id}', name: 'check_reponse')]
     // public function checkQuizz($id, CategorieRepository $repositoryCategorie, QuestionRepository $repositoryQuestion, ReponseRepository $repositoryReponse, Request $request): Response
