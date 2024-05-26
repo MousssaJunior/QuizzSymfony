c174166f745248486ead7cdd3a4b08bb69f65ef4
rka28 <karim.bara@epitech.eu>
2024-05-26 15:47:03 +0200

    add:historique des quizz

--- quizz-symfony/src/Controller/HistoriqueController.php
+++ quizz-symfony/src/Controller/HistoriqueController.php
@@ -1,20 +1,51 @@
 <?php
 
+// src/Controller/UserProfileController.php
+
+// src/Controller/HistoriqueController.php
+
+// src/Controller/HistoriqueController.php
+
+// src/Controller/HistoriqueController.php
+
+// src/Controller/HistoriqueController.php
+
 namespace App\Controller;
 
+use App\Entity\Historique;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class HistoriqueController extends AbstractController
 {
-    #[Route('/historique', name: 'app_historique')]
-    public function send(): Response
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
     {
-        return $this->render('historique/index.html.twig', [
-            'controller_name' => 'HistoriqueController',
-        ]);
+        $this->entityManager = $entityManager;
     }
 
-    
+    #[Route('/historique', name: 'user_historique')]
+    public function historique(): Response
+    {
+        $user = $this->getUser();
+        if (!$user instanceof UserInterface) {
+            throw $this->createAccessDeniedException();
+        }
+
+        $userId = $user->getId();
+        $history = $this->entityManager->getRepository(Historique::class)->findBy(
+            ['id_user' => $userId],
+            ['id' => 'DESC']
+        );
+
+        return $this->render('historique/index.html.twig', [
+            'user' => $user,
+            'history' => $history,
+        ]);
+    }
 }
+
