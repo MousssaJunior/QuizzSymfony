b354f8de5e4922c1f884d25fee8d61bbaf1d8379
moussa <moussa-junior.fofana@epitech.eu>
2024-05-26 17:52:05 +0200

    fix:remove dd from reponse controller

--- quizz-symfony/src/Controller/ReponseController.php
+++ quizz-symfony/src/Controller/ReponseController.php
@@ -44,7 +44,7 @@ class ReponseController extends AbstractController
             if ($expected && $expected->getReponseExpected() == 1) {
                 $resultat++;
             }
-            dd($expected->getReponseExpected());
+            
    
             $histo = new Historique();
             $histo->setIdUser($info_user);
