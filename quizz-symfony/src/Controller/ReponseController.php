1252b28bb9e186fe37a932abe4d59d1d4a652a04
moussa <moussa-junior.fofana@epitech.eu>
2024-05-26 10:28:26 +0200

    add:send to db info when a user do a quizz (historique)

--- quizz-symfony/src/Controller/ReponseController.php
+++ quizz-symfony/src/Controller/ReponseController.php
@@ -1,55 +1,69 @@
-<?php
-
+<?php 
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Repository\CategorieRepository;
 use App\Repository\QuestionRepository;
 use App\Repository\ReponseRepository;
-use App\Repository\HistoriqueRepository;
-use App\Repository\UsersRepository;
-use App\Entity\Historique;
-use App\Entity\Users;
-use App\Entity\Categorie;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-
-
-
+use App\Entity\Historique;
 
 class ReponseController extends AbstractController
 {
     #[Route('/reponse_back/{id_categorie}', name: 'app_reponse')]
-    public function GetResponses(Request $request,  CategorieRepository $repositoryCategorie, QuestionRepository $repositoryQuestion, ReponseRepository $repositoryReponse, $id_categorie): RedirectResponse
-    {   $reponse =  $request->get("question");
+    public function GetResponses(
+        Request $request,
+        CategorieRepository $repositoryCategorie,
+        QuestionRepository $repositoryQuestion,
+        ReponseRepository $repositoryReponse,
+        $id_categorie,
+        EntityManagerInterface $entityManager
+    ): RedirectResponse {
+      
+        $reponse = $request->get('question');
 
-        $keys = array_keys($reponse);    
-        $value = array_values($reponse);
-        $expected = $repositoryReponse->findby(['id' => $value] );
-        $resultat = 0;
-        $AllQuestions = $repositoryQuestion->countQuestions($id_categorie);
-        foreach($expected as $val){
-            if($val->getReponseExpected() == 1){
-               $resultat++;
-            }
-        } 
-        // $id_user = getid();
-         $user = new users;
-        $info_user = $this->getUser()->getId();
-        $categorie = $repositoryCategorie->findby(['id' => $id_categorie]);   
-        $categorie = $repositoryQuestion->findby(['id_categorie' => $id_user]);   
-        $name_categorie = $categorie[0]->getName();
-        $histo = new historique();
-         $histo->setIdUser($id_user);
-         $histo->setIdCategorie($id_categorie);
-         $histo->setNameCategorie($name_categorie);
-         $histo->setIdQuestion($keys);
-         $histo->setIdReponse($value);  
+    
         
+        $resultat = 0;
 
-            return $this->redirectToRoute('app_resultat', ['resultat' => $resultat, 'AllQuestion' => $AllQuestions[0]['COUNT(id)']]);
+    
+        $AllQuestions = $repositoryQuestion->countQuestions($id_categorie);
+
+        $info_user = $this->getUser()->getId();
+
+        $categorie = $repositoryCategorie->find($id_categorie);
+        $name_categorie = $categorie->getName();
+
+      
+        foreach ($reponse as $key => $value) {
+            $expected = $repositoryReponse->find($value);
+            if ($expected && $expected->getReponseExpected() == 1) {
+                $resultat++;
+            }
+
+   
+            $histo = new Historique();
+            $histo->setIdUser($info_user);
+            $histo->setIdCategorie($id_categorie);
+            $histo->setNameCategorie($name_categorie);
+            $histo->setIdQuestion((int)$key);  
+            $histo->setIdReponse((int)$value);  
+
+       
+            $entityManager->persist($histo);
+        }
+
+
+        $entityManager->flush();
+
+ 
+        return $this->redirectToRoute('app_resultat', [
+            'resultat' => $resultat,
+            'AllQuestion' => $AllQuestions[0]['COUNT(id)']
+        ]);
     }
-
 }
