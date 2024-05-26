0607555c48d4c07f1c021847616d7d0da2c3c6e0
rka28 <karim.bara@epitech.eu>
2024-05-26 18:18:30 +0200

    fix:historique id replace to name and add categorie

--- quizz-symfony/src/Controller/HistoriqueController.php
+++ quizz-symfony/src/Controller/HistoriqueController.php
@@ -1,29 +1,21 @@
 <?php
 
-// src/Controller/UserProfileController.php
-
-// src/Controller/HistoriqueController.php
-
-// src/Controller/HistoriqueController.php
-
-// src/Controller/HistoriqueController.php
-
-// src/Controller/HistoriqueController.php
-
 namespace App\Controller;
 
 use App\Entity\Historique;
+use App\Entity\Question;
+use App\Entity\Reponse;
+use App\Entity\Categorie; // Importer l'entité Catégorie
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\User\UserInterface;
 use App\Repository\CategorieRepository;
 use App\Repository\QuestionRepository;
 use App\Repository\ReponseRepository;
 use Symfony\Component\HttpFoundation\Request;
-
-
 
 class HistoriqueController extends AbstractController
 {
@@ -48,10 +40,32 @@ class HistoriqueController extends AbstractController
             ['id' => 'DESC']
         );
 
+        $historyData = [];
+        foreach ($history as $entry) {
+            /** @var Historique $entry */
+            $questionId = $entry->getIdQuestion();
+            $reponseId = $entry->getIdReponse();
+            $categorieId = $entry->getIdCategorie();
+            
+          
+            $question = $this->entityManager->getRepository(Question::class)->find($questionId);
+            $reponse = $this->entityManager->getRepository(Reponse::class)->find($reponseId);
+            $categorie = $this->entityManager->getRepository(Categorie::class)->find($categorieId);
+            
+       
+            if ($question && $reponse && $categorie) {
+                $historyData[] = [
+                    'categorie' => $categorie->getname(), 
+                    'question' => $question->getQuestion(),
+                    'reponse' => $reponse->getReponse(),
+                  
+                ];
+            }
+        }
+
         return $this->render('historique/index.html.twig', [
             'user' => $user,
-            'history' => $history,
+            'history' => $historyData,
         ]);
     }
 }
-
